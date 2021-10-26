bdc492b4c9eeb66d87daa301c4ace7496a8636d1
Josue <josue@pihuala.com.argit config --list>
2021-10-26 11:33:04 -0300

    clase 2 y 3

--- main.js
+++ main.js
@@ -1,22 +1,41 @@
-let topping = prompt("Que topping queres");
+let ingresarNombre = prompt("Ingrese su nombre, por favor.")
+let combo = prompt("Hola," + ingresarNombre +"." + "\nQue combo de shampoo solido quiere: \n -alfa \n -beta \n -gamma \n -delta");
 
-let precio = 0.00;
-var helado = 1.90;
-var precioFinal = 0.00;
+let precio = 0;
+var envio = 400;
+var precioFinal = 0;
 
-if (topping == "oreo") {
-    precio = 1;    
-} else if(topping == "kitkat"){
-    precio = 1.5;
-} else if(topping == "brownie"){
-    precio = 0.75;
-} else if(topping == "lacasitos"){
-    precio = 0.95;
+if (combo == "alfa") {
+    precio = 1450;
+} else if(combo == "beta"){
+    precio = 1450;
+} else if(combo == "gamma"){
+    precio = 1450;
+} else if(combo == "delta"){
+    precio = 1450;
 } else{
-    console.log("no tenemos ese topping");
+    console.log("no tenemos ese combo");
     precio = 0
 }
 
-precioFinal = helado + precio
+precioFinal = envio + precio
 
-console.log("el helado cuesta " + precioFinal + " en pesos.");
+alert("El combo cuesta " + precioFinal + " en pesos." + "\n Incluye " + envio + " de envio." );
+
+
+let ingreseHorario = prompt("Ingrese horario que desea recibir el envio: \n -mañana  \n -tarde");
+var mañana = ["Lunes"];
+var tarde = ["Jueves"];
+while (ingreseHorario != "ESC") {
+    switch (ingreseHorario) {
+    case "mañana":
+        alert(ingresarNombre + " su pedido estara llegando el " + mañana);
+        break;
+    case "tarde":
+        alert(ingresarNombre + " su pedido estara llegando el " + tarde);
+        break;
+    default:
+        alert(ingresarNombre + ", por favor indique un horario");
+    }
+    ingreseHorario = prompt("Consulte otro horario: " + horarioMañana + horarioTarde);
+}
